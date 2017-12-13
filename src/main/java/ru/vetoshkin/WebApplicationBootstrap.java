a97e593c9a303ae108452e81faa903d1e3e84be2
Skuil <topkdr4@mail.ru>
2017-12-13 23:19:00 +0500

    Карточка лыжника

--- src/main/java/ru/vetoshkin/WebApplicationBootstrap.java
+++ src/main/java/ru/vetoshkin/WebApplicationBootstrap.java
@@ -1,26 +1,19 @@
 package ru.vetoshkin;
-import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import java.util.Arrays;
-
-
-
 
 
 /**
  * Ветошкин А.В. РИС-16бзу
- * */
+ */
 @WebListener
 public class WebApplicationBootstrap implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        JAXRSServerFactoryBean factoryBean = new JAXRSServerFactoryBean();
-        factoryBean.setResourceClasses(Arrays.asList(Test.class));
-        factoryBean.create();
+
     }
 
 
